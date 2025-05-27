3f35820ec997dbffdc1ec99e36d7bbdf52708b5b
Troels Henriksen <athas@sigkill.dk>
2025-05-27 13:16:30 +0200

    CUDA fixes.

--- inttables.cu
+++ inttables.cu
@@ -8,6 +8,8 @@
 #include <thrust/sequence.h>
 #include <thrust/transform.h>
 
+#include <cooperative_groups.h>
+
 #include <cmath>
 #include <cstddef>
 #include <iostream>
@@ -16,10 +18,19 @@
 #include "data.hpp"
 #include "timing.hpp"
 
-int main(int argc, char** argv) {
-  using Key   = int64_t;
-  using Value = int;
+using Key   = int64_t;
+using Value = int;
 
+
+template <typename MapRef, typename InputIterator, typename OutputIterator>
+__global__ void scalar_find(MapRef set, InputIterator keys, std::size_t n, OutputIterator found) {
+  int64_t i = blockDim.x * blockIdx.x + threadIdx.x;
+
+  auto [j, val] = *set.find(*(keys + i));
+  found[i] = val;
+}
+
+int main(int argc, char** argv) {
   if (argc != 3) {
     std::cerr << "Usage: " << argv[0] << " KEYS VALUES" << std::endl;
     return 1;
@@ -30,6 +41,7 @@ int main(int argc, char** argv) {
 
   auto [keys, keys_shape] = read_futhark_array<int64_t,1>(keys_fname);
   auto [vals, vals_shape] = read_futhark_array<int32_t,1>(vals_fname);
+
 
   if (keys_shape[0] != vals_shape[0]) {
     throw std::runtime_error("Mismatch in number of keys and values");
@@ -48,20 +60,19 @@ int main(int argc, char** argv) {
   auto map = cuco::static_map{
     capacity,
     cuco::empty_key{empty_key_sentinel},
-    cuco::empty_value{empty_value_sentinel}
+    cuco::empty_value{empty_value_sentinel},
+    cuda::std::equal_to<Key>(),
+    cuco::linear_probing<1,cuco::default_hash_function<Key>>()
   };
 
-  thrust::device_vector<Key> insert_keys(num_keys);
-  thrust::sequence(keys.begin(), keys.end(), 0);
-  thrust::device_vector<Value> insert_values(num_keys);
-  thrust::sequence(vals.begin(), vals.end(), 0);
+  thrust::device_vector<Key> insert_keys(keys.begin(), keys.end());
+  thrust::device_vector<Value> insert_values(vals.begin(), vals.end());
   auto pairs = thrust::make_transform_iterator
     (thrust::counting_iterator<std::size_t>{0},
      cuda::proclaim_return_type<cuco::pair<Key, Value>>
      ([keys = insert_keys.begin(), values = insert_values.begin()] __device__(auto i) {
        return cuco::pair<Key, Value>{keys[i], values[i]};
      }));
-
 
   int insertAvgTime = measureAverageExecutionTime
     (2.0,
@@ -75,19 +86,42 @@ int main(int argc, char** argv) {
 
   thrust::device_vector<Value> found_values(num_keys);
 
-  int lookupAvgTime = measureAverageExecutionTime
-    (2.0,
-     [&]() {
-       map.find(insert_keys.begin(), insert_keys.end(), found_values.begin());
-       cudaDeviceSynchronize();
-     });
+  {
+    int lookupAvgTime = measureAverageExecutionTime
+      (2.0,
+       [&]() {
+         map.find(insert_keys.begin(), insert_keys.end(), found_values.begin());
+         cudaDeviceSynchronize();
+       });
 
-  std::cout << "       lookup: " << lookupAvgTime << "μs" << std::endl;
+    std::cout << "       lookup: " << lookupAvgTime << "μs" << std::endl;
 
-  bool const all_values_match =
-    thrust::equal(found_values.begin(), found_values.end(), insert_values.begin());
+    bool const all_values_match =
+      thrust::equal(found_values.begin(), found_values.end(), insert_values.begin());
 
-  if (!all_values_match) {
-    return 1;
+    if (!all_values_match) {
+      return 1;
+    }
+  }
+
+  {
+    const size_t BLOCK_SIZE = 256;
+    size_t grid_size = (num_keys + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    int scalarLookupAvgTime = measureAverageExecutionTime
+      (2.0,
+       [&]() {
+         scalar_find<<<grid_size,BLOCK_SIZE>>>(map.ref(cuco::find), insert_keys.begin(), num_keys, found_values.begin());
+         cudaDeviceSynchronize();
+       });
+
+    std::cout << "scalar lookup: " << scalarLookupAvgTime << "μs" << std::endl;
+
+    bool const all_values_match =
+      thrust::equal(found_values.begin(), found_values.end(), insert_values.begin());
+
+    if (!all_values_match) {
+      return 1;
+    }
   }
 }
